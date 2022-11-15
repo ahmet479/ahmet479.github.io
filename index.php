6109fcb6e210896896ad73243383077d820ead49
ahmet479 <115145728+ahmet479@users.noreply.github.com>
2022-11-15 11:21:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title> Ilk Web Sayfasi </title>
+</head>
+<body>
+    <p>Bugun 8 Kasim 2022.Gunlerden sali. Mevsim sonbahar. Hava soguk.!</p>
+    <p>Bu iki paragraf. Ilki ile arasinda bosluk olacak.</p>
+</body>
+<html>
